b25d85c5daa649485e67f38beae6f23f1e080cfe
diaomingkai <diaomingkai@126.com>
2016-02-03 17:32:51 +0800

    添加相关包

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 /**
  * Created by diaomingkai on 2016-2-3.
  */
-public class Test {
+public class Main {
 }
---
+++ src/main/java/com/dx/orm/sqlserver/Test.java
@@ -0,0 +1,7 @@
+package com.dx.orm.sqlserver;
+
+/**
+ * Created by diaomingkai on 2016-2-3.
+ */
+public class Test {
+}
