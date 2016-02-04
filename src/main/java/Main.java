1055a0befa4e25b426926ccda8fff9c3718894f5
diaomingkai <diaomingkai@126.com>
2016-02-04 08:54:15 +0800

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import com.dx.orm.idao.IDao;
+
 /**
  * Created by diaomingkai on 2016-2-3.
  */
 public class Main {
+
+
+    public static void main(String[] args) {
+        try {
+            IDao iDao = (IDao)Class.forName("com.dx.orm.sqlserver.Db").newInstance();
+            System.out.print(iDao.insert());
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/dx/orm/oracle/Db.java
+++ src/main/java/com/dx/orm/oracle/Db.java
@@ -1,7 +1,31 @@
 package com.dx.orm.oracle;
 
+import com.dx.orm.idao.IDao;
+
+import java.util.List;
+
 /**
  * Created by diaomingkai on 2016-2-3.
  */
-public class Db {
+public class Db implements IDao {
+
+    public <T> int insert(T entity) {
+        return 0;
+    }
+
+    public <T> int update(T entity) {
+        return 0;
+    }
+
+    public <T> T one(T entity) {
+        return null;
+    }
+
+    public <T> List<T> list(T entity) {
+        return null;
+    }
+
+    public int delete(String id) {
+        return 0;
+    }
 }
